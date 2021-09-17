872d1395d7fa206750334ae8fbb0e97db259e775
Arbi Syarifudin <arbisyarifudin@gmail.com>
2021-09-17 21:59:13 +0700

    edit: struktur header comp

--- components/Header.vue
+++ components/Header.vue
@@ -3,36 +3,7 @@
     id="header"
     :class="['header', haveVariant ? 'variant-' + this.variant : '']"
   >
-    <b-navbar toggleable="lg" type="dark" variant="transparent">
-      <b-container>
-        <b-navbar-brand to="/"><img src="/logo-ujiaja.png" /></b-navbar-brand>
-        <b-navbar-toggle target="navbarheader"></b-navbar-toggle>
-        <b-collapse id="navbarheader" is-nav>
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto align-items-center">
-            <b-nav-item-dropdown text="Produk UjiAja">
-              <template #button-content>
-                Produk UjiAja <i class="fas fa-chevron-down ml-1"></i>
-              </template>
-              <b-dropdown-item href="/produk/tryout">TryOut</b-dropdown-item>
-              <b-dropdown-item href="/produk/les-privat"
-                >Les Privat</b-dropdown-item
-              >
-            </b-nav-item-dropdown>
-            <b-nav-item href="/tentang">Tentang UjiAja</b-nav-item>
-            <b-nav-item href="/untuk-tutor">Untuk Tutor</b-nav-item>
-            <b-nav-item href="/untuk-siswa">Untuk Siswa</b-nav-item>
-            <b-nav-item href="/untuk-orangtua">Untuk Orangtua</b-nav-item>
-            <b-button
-              href="/masuk"
-              variant="outline-primary"
-              class="mt-5 mt-lg-0"
-              >Masuk</b-button
-            >
-          </b-navbar-nav>
-        </b-collapse>
-      </b-container>
-    </b-navbar>
+    <NavHeader />
     <Hero :props="heroData" v-if="heroData" />
   </header>
 </template>
---
+++ components/NavHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="transparent" class="mx-lg-5">
+    <b-container>
+      <b-navbar-brand to="/"><img src="/logo-ujiaja.png" /></b-navbar-brand>
+      <b-navbar-toggle target="navbarheader"></b-navbar-toggle>
+      <b-collapse id="navbarheader" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto align-items-center">
+          <b-nav-item-dropdown text="Produk UjiAja">
+            <template #button-content>
+              Produk UjiAja <i class="fas fa-chevron-down ml-1"></i>
+            </template>
+            <b-dropdown-item href="/produk/tryout">TryOut</b-dropdown-item>
+            <b-dropdown-item href="/produk/les-privat"
+              >Les Privat</b-dropdown-item
+            >
+          </b-nav-item-dropdown>
+          <b-nav-item href="/tentang">Tentang UjiAja</b-nav-item>
+          <b-nav-item href="/untuk-tutor">Untuk Tutor</b-nav-item>
+          <b-nav-item href="/untuk-siswa">Untuk Siswa</b-nav-item>
+          <b-nav-item href="/untuk-orangtua">Untuk Orangtua</b-nav-item>
+          <b-button href="/masuk" variant="outline-primary" class="mt-5 mt-lg-0"
+            >Masuk</b-button
+          >
+        </b-navbar-nav>
+      </b-collapse>
+    </b-container>
+  </b-navbar>
+</template>
