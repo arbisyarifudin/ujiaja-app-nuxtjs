427e8b77d313ff571bfc620e12a3d0ab8d24160c
Arbi Syarifudin <arbisyarifudin@gmail.com>
2021-09-17 21:59:30 +0700

    add: page -> karir

---
+++ components/HeaderKarir.vue
@@ -0,0 +1,44 @@
+<template>
+  <header
+    :class="[
+      'header headerdua-karir',
+      haveVariant ? 'variant-' + this.variant : '',
+    ]"
+  >
+    <NavHeader />
+    <div class="hero" id="hero" style="min-height: 400px">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <div class="justify-content-center">
+              <h2 class="m-0 mb-4" style="font-size: 40px">
+                Bergabunglah Dengan UjiAja!
+              </h2>
+              <h4 class="m-0">
+                Mari bergabung bersama kami untuk majukan teknologi pendidikan
+                Indonesia
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ["heroData", "variant"],
+  data() {
+    return {};
+  },
+  computed: {
+    haveVariant() {
+      if (this.variant || this.variant == "") {
+        return true;
+      }
+      return false;
+    },
+  },
+};
+</script>
---
+++ pages/karir/index.vue
@@ -0,0 +1,135 @@
+<template>
+  <div>
+    <HeaderKarir />
+    <div class="program-studi mb-5 mt-n5">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-9 col-md-8">
+            <div class="cari karir text-center pt-5 pb-5 mt-n3">
+              <h3 class="mt-3">Mengapa Bergabung Dengan Kami?</h3>
+              <div class="text-left">
+                <p>
+                  Masa depan pendidikan bergantung pada dialog berkelanjutan
+                  antara pendidik dan lembaga pendidikan, dan profesional di
+                  dunia teknologi. EdTech adalah studi dan praktik etis untuk
+                  memfasilitasi pembelajaran dan meningkatkan kinerja dengan
+                  menciptakan, menggunakan, dan mengelola proses dan sumber daya
+                  teknologi yang sesuai. Janji teknologi pendidikan yang
+                  berkembang memberikan peluang menarik untuk menerapkan
+                  keterampilan Anda dalam lingkungan pendidikan dengan dampak
+                  yang benar-benar global. <br /><br />
+                  Jika Anda merasa Anda adalah pemain top di beberapa area
+                  tetapi tidak dapat menemukan lowongan, silakan kirim resume
+                  Anda ke karir@ujiaja.com
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-12 text-center">
+          <div class="karir my-4">
+            <h2 class="pb-3 m-0">Kesempatan Berkarir</h2>
+            <p>Cari tahu bagaimana Anda dapat berkontribusi</p>
+          </div>
+
+          <div class="row d-flex justify-content-center">
+            <div class="col-md-4">
+              <div class="card card-karir">
+                <div class="card-body text-left p-0">
+                  <div class="card-content px-4">
+                    <h3 class="card-judul mt-4 mb-2">
+                      Backend Software Engineer
+                    </h3>
+                    <p class="card-text pb-3">
+                      Purna Waktu - WFH, Dimanapun
+                      <br />
+                      Kisaran Gaji: Rp.4.500.000 - Rp.15.000.000
+                    </p>
+                    <a href="/karir/1245-graphic-designer" class="karir-link"
+                      >Detail <i class="fa fa-chevron-right"></i
+                    ></a>
+                  </div>
+                  <div class="d-flex justify-content-end pb-4 pr-4 m-0 icon">
+                    <!-- <img src="assets/img/IconKenapa/kariricon.png" class="" /> -->
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card card-karir">
+                <div class="card-body text-left p-0">
+                  <div class="card-content px-4">
+                    <h3 class="card-judul my-4">SEO Specialist</h3>
+                    <p class="card-text pb-4">
+                      Purna Waktu - WFH / WFO <br />Kisaran Gaji: Rp.4.500.000 -
+                      Rp.6.500.000
+                    </p>
+                    <a href="/karir/1245-graphic-designer" class="karir-link"
+                      >Detail <i class="fa fa-chevron-right"></i
+                    ></a>
+                  </div>
+                  <div class="d-flex justify-content-end pb-4 pr-4 m-0 icon">
+                    <!-- <img src="assets/img/IconKenapa/kariricon2.png" class="" /> -->
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card card-karir">
+                <div class="card-body text-left p-0">
+                  <div class="card-content px-4">
+                    <h3 class="card-judul my-4">Graphic Designer</h3>
+                    <p class="card-text pb-4">
+                      Magang - WFH / WFO, Indonesia <br />Gaji: Rp. 2.200.000
+                    </p>
+                    <a href="/karir/1245-graphic-designer" class="karir-link"
+                      >Detail <i class="fa fa-chevron-right"></i
+                    ></a>
+                  </div>
+                  <div class="d-flex justify-content-end pb-4 pr-4 m-0 icon">
+                    <!-- <img src="assets/img/IconKenapa/kariricon3.png" class="" /> -->
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <button
+            class="btn btn-outline-primary px-4 mt-5 mb-3"
+            style="border-radius: 25px"
+          >
+            Lihat Semua Peluang Karir
+          </button>
+        </div>
+      </div>
+      <div
+        class="col-lg-12 text-center mb-5 mt-4"
+        style="background-color: #f5f3ff"
+      >
+        <div class="row bg-white shadow-sm">
+          <div class="col-md-5 text-right">
+            <img src="/karir-bawah.png" alt="" />
+          </div>
+          <div class="col-md-7 text-left">
+            <h3 class="mb-3" style="font-size: 36px">
+              Ayo Bergabung di UjiAja!
+            </h3>
+            <p>Kirim cover letter dan CV kamu ke karir@ujiaja.id</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      bodyAttrs: {
+        class: "",
+      },
+    };
+  },
+};
+</script>
