9a511cfcea785cbac6bec0188d04c4e1e7e41990
Arbi Syarifudin <arbisyarifudin@gmail.com>
2021-09-17 20:12:27 +0700

    upd: nav url on footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -24,8 +24,8 @@
         <div class="mb-3 col-lg-3 col-sm-4 col-6">
           <ul class="list-unstyled">
             <li><h4>Tentang UjiAja</h4></li>
-            <li><a class="li" href="#">Tentang UjiAja</a></li>
-            <li><a class="li" href="#">Karir</a></li>
+            <li><a class="li" href="/tentang">Tentang UjiAja</a></li>
+            <li><a class="li" href="/karir">Karir</a></li>
             <li><a class="li" href="/untuk-tutor">Untuk Tutor</a></li>
             <li><a class="li" href="/untuk-siswa">Untuk Siswa</a></li>
             <li><a class="li" href="/untuk-orangtua">Untuk Orang Tua</a></li>
@@ -34,13 +34,17 @@
         <div class="mb-3 col-lg-3 col-sm-4 col-6">
           <ul class="list-unstyled">
             <li><h4>Produk UjiAja</h4></li>
-            <li><a class="li" href="#">Tryout UTBK</a></li>
-            <li><a class="li" href="#">Tryout Asesmen Nasional</a></li>
-            <li><a class="li" href="#">Tryout ASPD</a></li>
+            <li><a class="li" href="/produk/tryout-utbk">Tryout UTBK</a></li>
+            <li>
+              <a class="li" href="/produk/tryout-asesmen-nasional"
+                >Tryout Asesmen Nasional</a
+              >
+            </li>
+            <li><a class="li" href="/produk/tryout-asp">Tryout ASPD</a></li>
             <li>
               <a class="li" href="#">Ujian Kenaikan Tingkat Tentor (UKTT)</a>
             </li>
-            <li><a class="li" href="#">Les Privat</a></li>
+            <li><a class="li" href="/produk/les-privat">Les Privat</a></li>
           </ul>
         </div>
         <div class="mb-3 col-lg-3 col-sm-4 col-12">
@@ -75,9 +79,13 @@
                 </li>
               </ul>
             </li>
-            <li><a class="li" href="#">FAQ / Bantuan</a></li>
-            <li><a class="li" href="#">Syarat dan Ketentuan</a></li>
-            <li><a class="li" href="#">Kebijakan Privasi</a></li>
+            <li><a class="li" href="/bantuan">FAQ / Bantuan</a></li>
+            <li>
+              <a class="li" href="/syarat-ketentuan">Syarat dan Ketentuan</a>
+            </li>
+            <li>
+              <a class="li" href="/kebijakan-privasi">Kebijakan Privasi</a>
+            </li>
           </ul>
         </div>
       </div>
