629fbe7737258c853838ffb90c277a976e11f4c5
Arbi Syarifudin <arbisyarifudin@gmail.com>
2021-09-17 20:33:13 +0700

    add: page -> syarat ketentuan

---
+++ pages/syarat-ketentuan.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+    <Header variant="melengkung no-hero primary" />
+    <div class="page-content kebijakan-privasi bg-white">
+      <div class="">
+        <h2 class="page-title">Kebijakan Privasi</h2>
+        <div class="page-text">
+          <h3>A. Syarat & Ketentuan Umum</h3>
+
+          <p>
+            Selamat Datang di SSCI, sebuah platform pembelajaran online yang
+            dioperasikan oleh Intersolusi, yang membantu melengkapi proses
+            belajar dan mengajar yang diharapkan mampu membantu mencapai
+            keunggulan akademik.
+          </p>
+
+          <p>
+            Syarat dan Ketentuan SSCI terdiri dari: setiap ketentuan (yang dapat
+            diubah dan/atau dilengkapi dari waktu ke waktu) yang terdapat di
+            setiap perjanjian terpisah yang disepakati oleh Kelas Pintar dan
+            pengguna dari Platform (sebagaimana didefinisikan di bawah ini) dan
+            juga bagian-bagian berikut ini: Kebijakan Penggunaan (sebagaimana
+            didefinisikan di bawah ini), Kebijakan Privasi (sebagaimana
+            didefinisikan di bawah ini), Kebijakan Keamanan (sebagaimana
+            didefinisikan di bawah ini) dan Kebijakan Keselamatan Anak
+            (sebagaimana didefinisikan di bawah ini), yang semuanya merupakan
+            satu kesatuan yang saling melengkapi dan tidak dapat dipisahkan.
+          </p>
+
+          <p>
+            Dengan menggunakan Platform (sebagaimana didefinisikan di bawah
+            ini), Anda setuju untuk menerima dan menyepakati setiap
+            ketentuan-ketentuan yang terdapat di dalam Syarat dan Ketentuan SSCI
+            beserta segala perubahan dan/atau tambahannya.
+          </p>
+
+          <p>
+            Setiap Syarat dan Ketentuan SSCI yang ditetapkan harus ditafsirkan
+            sedemikian rupa agar efektif dan berlaku berdasarkan hukum yang
+            berlaku. Jika terdapat suatu syarat dan ketentuan yang dianggap
+            tidak sah dan tidak dapat diberlakukan, maka ketidaksahan atau
+            ketidakberlakuan tersebut tidak akan mempengaruhi syarat dan
+            ketentuan lainnya di dalam Setiap Syarat dan Ketentuan SSCI ini.
+          </p>
+
+          <p>
+            Syarat dan Ketentuan SSCI ini, termasuk Kebijakan Penggunaan,
+            Kebijakan Privasi, Kebijakan Keamanan dan Kebijakan Keselamatan Anak
+            diatur oleh dan ditafsirkan sesuai dengan hukum Republik Indonesia
+            dan setiap sengketa yang timbul dari penggunaan Platform harus
+            diselesaikan secara musyawarah, dan apabila tidak dapat diselesaikan
+            secara musyawarah dalam waktu 30 (tiga puluh) hari kerja sejak
+            adanya pemberitahuan formal mengenai adanya sengketa, maka sengketa
+            yang timbul harus diselesaikan di Badan Arbitrase Nasional Indonesia
+            (BANI) yang didirikan pada tahun 1977. Anda tidak akan mengalihkan
+            setiap hak dan kewajiban yang muncul berdasarkan Kebijakan
+            Penggunaan ini, secara keseluruhan atau sebagian, baik secara
+            sukarela atau pengoperasian hukum, tanpa persetujuan tertulis
+            sebelumnya dari SSCI. Setiap pengalihan yang diakui tersebut tanpa
+            persetujuan Kelas Pintar menjadi batal demi hukum dan tidak memiliki
+            kekuatan hukum.
+          </p>
+
+          <h4>Kebijakan Penggunaan</h4>
+
+          <p>
+            Yang dimaksud dengan Kebijakan Penggunaan adalah seluruh syarat dan
+            ketentuan (beserta seluruh perubahan dan/atau penambahannya) terkait
+            dengan penggunaan situs SSCI yang beralamat di: www.ssci.id
+            (“Situs”), dan seluruh situs terkait yang ditautkan ke Situs oleh
+            SSCI di seluruh dunia serta aplikasi mobile, atau produk, piranti
+            lunak (software), file data dan layanan apa pun yang tersedia untuk
+            dan kepada user di, dari, atau melalui situs atau aplikasi yang
+            terkait dengan domain atau sub-domain dari Situs (secara kolektif,
+            “Platform”). Platform ini adalah milik SSCI. DENGAN MENGGUNAKAN
+            PLATFORM (TERMASUK SITUS INI), ANDA MENYETUJUI SETIAP DAN SELURUH
+            DARI KEBIJAKAN PENGGUNAAN INI; JIKA ANDA TIDAK MENYETUJUI, MOHON
+            BERHENTI UNTUK MENGGUNAKAN ATAU MENGUNJUNGI PLATFORM (TERMASUK SITUS
+            INI).
+          </p>
+
+          <p>
+            SSCI berhak, atas kebijakan sendiri secara wajar, untuk mengubah,
+            memodifikasi, menambah atau menghapus setiap bagian dari Kebijakan
+            Penggunaan kapan saja dan dengan alasan apa pun, tanpa pemberitahuan
+            sebelumnya (termasuk ketentuan terkait harga Produk dan/atau Layanan
+            (sebagaimana didefinisikan pada Bagian G Kebijakan Penggunaan ini).
+            Setiap pengguna Platform (“Anda” atau “Pengguna”) bertanggung jawab
+            untuk memeriksa Kebijakan Penggunaan secara berkala untuk melihat
+            perubahan pada Kebijakan Penggunaan. Dengan tetap menggunakan
+            Platform setelah adanya perubahan berarti Anda telah menerima dan
+            menyetujui perubahan tersebut. Selama Anda mematuhi Kebijakan
+            Penggunaan ini, SSCI memberi Anda hak istimewa yang bersifat
+            pribadi, non-eksklusif, tidak dapat dialihkan, terbatas untuk masuk
+            dan menggunakan Platform.
+          </p>
+        </div>
+      </div>
+    </div>
+    <Footer class="bg-white" />
+  </div>
+</template>
