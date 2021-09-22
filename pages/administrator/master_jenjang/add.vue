1ee3792260b86a728162df637f3dbf8acda2982e
Arbi Syarifudin <arbisyarifudin@gmail.com>
2021-09-22 21:10:56 +0700

    add CRUD master jenjang

---
+++ pages/administrator/master_jenjang/add.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="container-fluid crud">
+    <form @submit.prevent="validateForm">
+      <div class="row d-flex no-gutters">
+        <div class="col-md-12 dashboard">
+          <h2 class="dash-label">Tambah Jenjang</h2>
+          <p>
+            Ayo, buat data master jenjang sekarang juga untuk merelasikan data
+            sekolah dan perguruan tinggi.
+          </p>
+        </div>
+        <div class="col-md-12 crud-body">
+          <div class="form-user mt-3">
+            <div class="form-group reg-siswa">
+              <label for="text">Nama Jenjang</label>
+              <input
+                type="text"
+                class="form-control"
+                id="text"
+                placeholder="Isi Nama Jenjang"
+                v-model="form.nama_jenjang"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="crud-footer d-flex justify-content-end mt-4">
+          <nuxt-link
+            to="/administrator/master_jenjang"
+            class="btn btn-outline-secondary mr-2"
+          >
+            Kembali
+          </nuxt-link>
+          <button type="submit" class="btn btn-primary" :disabled="loading">
+            <b-spinner small class="mr-1" v-if="loading"></b-spinner>
+            Simpan
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "admin",
+  data() {
+    return {
+      loading: false,
+      dataMaster: {
+        jenjang: [],
+      },
+      form: {
+        nama_jenjang: "",
+      },
+    };
+  },
+  methods: {
+    validateForm() {
+      console.log(this.form);
+      if (!this.form.nama_jenjang) {
+        this.$bvToast.toast("Mohon lengkapi form dengan benar!", {
+          title: "Peringatan",
+          variant: "warning",
+          solid: true,
+          autoHideDelay: 2000,
+        });
+        return;
+      }
+      this.submitData("jenjang");
+    },
+    submitData(type) {
+      this.loading = true;
+      this.$axios
+        .$post(`/api/${type}/create`, this.form)
+        .then((res) => {
+          console.log(res);
+          if (res.success) {
+            this.$root.$bvToast.toast("Data " + type + " berhasil ditambah.", {
+              title: "Sukses",
+              variant: "success",
+              solid: true,
+              autoHideDelay: 3000,
+            });
+            this.$router.replace("/administrator/master_jenjang");
+          }
+          return true;
+        })
+        .catch((err) => {
+          console.log(err);
+          this.catchError(err);
+        })
+        .finally(() => (this.loading = false));
+    },
+  },
+};
+</script>
---
+++ pages/administrator/master_jenjang/edit/_id.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="container-fluid crud">
+    <form @submit.prevent="validateForm">
+      <div class="row d-flex no-gutters">
+        <div class="col-md-12 dashboard">
+          <h2 class="dash-label">Ubah Jenjang</h2>
+          <!-- <p>
+            Ayo, buat data master kelas sekarang juga untuk merelasikan data
+            siswa.
+          </p> -->
+        </div>
+        <div class="col-md-12 crud-body">
+          <div class="form-user mt-3">
+            <div class="form-group reg-siswa">
+              <label for="text">Nama Jenjang</label>
+              <input
+                type="text"
+                class="form-control"
+                id="text"
+                placeholder="Isi Nama Jenjang"
+                v-model="form.nama_jenjang"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="crud-footer d-flex justify-content-end mt-4">
+          <nuxt-link
+            to="/administrator/master_jenjang"
+            class="btn btn-outline-secondary mr-2"
+          >
+            Kembali
+          </nuxt-link>
+          <button type="submit" class="btn btn-primary" :disabled="loading">
+            <b-spinner small class="mr-1" v-if="loading"></b-spinner>
+            Simpan
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "admin",
+  data() {
+    return {
+      loading: false,
+      selectedId: this.$route.params.id,
+      dataMaster: {
+        jenjang: [],
+      },
+      form: {
+        nama_jenjang: "",
+      },
+    };
+  },
+  mounted() {
+    if (!this.$route.params.id)
+      return this.$router.push("/administrator/master_jenjang");
+    this.getDetail("jenjang", this.$route.params.id);
+  },
+  methods: {
+    validateForm() {
+      console.log(this.form);
+      if (!this.form.nama_jenjang) {
+        this.$bvToast.toast("Mohon lengkapi form dengan benar!", {
+          title: "Peringatan",
+          variant: "warning",
+          solid: true,
+          autoHideDelay: 2000,
+        });
+        return;
+      }
+      this.submitData("jenjang");
+    },
+    submitData(type) {
+      this.loading = true;
+      this.$axios
+        .$put(`/api/${type}/update/${this.$route.params.id}`, this.form)
+        .then((res) => {
+          console.log(res);
+          if (res.success) {
+            this.$root.$bvToast.toast(
+              "Data " + type + " berhasil diperbarui.",
+              {
+                title: "Sukses",
+                variant: "success",
+                solid: true,
+                autoHideDelay: 3000,
+              }
+            );
+            this.$router.replace("/administrator/master_jenjang");
+          }
+          return true;
+        })
+        .catch((err) => {
+          console.log(err);
+          this.catchError(err);
+        })
+        .finally(() => (this.loading = false));
+    },
+    getDetail(type, id) {
+      this.loading = true;
+      this.$axios
+        .$get(`/api/${type}/find/${id}`)
+        .then((res) => {
+          console.log(res);
+          if (res.success) {
+            this.dataDetail = res.data;
+            this.form = {
+              id_jenjang: this.dataDetail.id_jenjang,
+              nama_jenjang: this.dataDetail.nama_jenjang,
+            };
+          }
+          return true;
+        })
+        .catch((err) => {
+          console.log(err);
+          this.catchError(err);
+        })
+        .finally(() => (this.loading = false));
+    },
+  },
+};
+</script>
