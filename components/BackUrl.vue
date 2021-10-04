e40f05a236118959de06629338a01e40599db754
Arbi Syarifudin <arbisyarifudin@gmail.com>
2021-10-05 02:47:44 +0700

    add backurl component

---
+++ components/BackUrl.vue
@@ -0,0 +1,16 @@
+<template>
+  <button
+    type="button"
+    @click.prevent="url ? $router.push(url) : $router.go(-1)"
+    class="btn btn-outline-secondary mr-2 square"
+  >
+    <b-icon icon="arrow-left" class="mr-1"></b-icon>
+    Kembali
+  </button>
+</template>
+
+<script>
+export default {
+  props: ["url"]
+};
+</script>
