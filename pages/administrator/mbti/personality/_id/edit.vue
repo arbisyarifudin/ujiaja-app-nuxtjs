8b12a9c83193e51865a08210d8e8c5933f1249f5
Arbi Syarifudin <arbisyarifudin@gmail.com>
2021-10-09 16:11:42 +0700

    create: admin > CRUD kepribadian mod (WIP)

---
+++ pages/administrator/mbti/personality/_id/edit.vue
@@ -0,0 +1,235 @@
+<template>
+  <div class="container-fluid crud">
+    <form @submit.prevent="validateForm">
+      <div class="row d-flex no-gutters">
+        <div class="col-md-12 dashboard">
+          <h2 class="dash-label">
+            <b-spinner type="grow" class="mr-2" v-if="loading" />
+            Tambah Kepribadian
+          </h2>
+          <p>
+            Ayo, buat data master kepribadian sekarang juga untuk merelasikan
+            data dimensi sifat dasar.
+          </p>
+        </div>
+        <div class="col-md-12 crud-body">
+          <div class="form-user mt-3 row">
+            <div class="form-group reg-siswa col-md-6">
+              <label for="nama_singkat"
+                >Nama Kepribadian (Singakatan) <code>*</code></label
+              >
+              <input
+                type="text"
+                class="form-control"
+                id="nama_singkat"
+                name="nama_singkat"
+                placeholder="Tulis Nama Singkatan"
+                v-model="form.nama_singkat"
+                disabled
+              />
+            </div>
+            <div class="form-group reg-siswa col-md-6">
+              <label for="nama_panjang"
+                >Nama Kepribadian (Kepanjangan) <code>*</code></label
+              >
+              <input
+                type="text"
+                class="form-control"
+                id="nama_panjang"
+                name="nama_panjang"
+                placeholder="Tulis Nama Kepanjangan"
+                v-model="form.nama_panjang"
+                disabled
+              />
+            </div>
+          </div>
+          <div class="form-user">
+            <div class="form-group reg-siswa">
+              <label for="jenis_kepribadian"
+                >Jenis Kepribadian <code>*</code></label
+              >
+              <input
+                type="jenis_kepribadian"
+                class="form-control"
+                id="jenis_kepribadian"
+                name="jenis_kepribadian"
+                placeholder="Jenis Kepribadian"
+                v-model="form.jenis"
+              />
+            </div>
+          </div>
+          <div class="form-user">
+            <div class="form-group reg-user">
+              <label for="deskripsi_kepribadian"
+                >Detail / Kelebihan <code>*</code></label
+              >
+              <client-only>
+                <VueEditor
+                  id="deskripsi_kepribadian"
+                  v-model="form.deskripsi"
+                  :editor-toolbar="customToolbar"
+                />
+              </client-only>
+            </div>
+          </div>
+          <div class="form-user">
+            <div class="form-group reg-user">
+              <label for="saran_kepribadian"
+                >Saran / Masukan <code>*</code></label
+              >
+              <client-only>
+                <VueEditor
+                  id="saran_kepribadian"
+                  v-model="form.saran"
+                  :editor-toolbar="customToolbar"
+                />
+              </client-only>
+            </div>
+          </div>
+          <div class="form-user">
+            <div class="form-group reg-siswa">
+              <label for="profesi_kepribadian">Profesi <code>*</code></label>
+              <input
+                type="profesi_kepribadian"
+                class="form-control"
+                id="profesi_kepribadian"
+                name="profesi_kepribadian"
+                placeholder="Tulis Profesi yang Cocok dari Kepribadian"
+                v-model="form.profesi"
+              />
+            </div>
+          </div>
+          <div class="form-user">
+            <div class="form-group reg-siswa">
+              <label for="partner_kepribadian"
+                >Partner Alami <code>*</code></label
+              >
+              <input
+                type="partner_kepribadian"
+                class="form-control"
+                id="partner_kepribadian"
+                name="partner_kepribadian"
+                placeholder="Tulis Partner Alami untuk Kepribadian"
+                v-model="form.partner"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="crud-footer d-flex justify-content-end mt-4">
+          <nuxt-link
+            to="/administrator/mbti/personality"
+            class="btn btn-outline-secondary mr-2"
+          >
+            Kembali
+          </nuxt-link>
+          <button type="submit" class="btn btn-primary" :disabled="loading">
+            <b-spinner small class="mr-1" v-if="loading"></b-spinner>
+            Simpan
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import vSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
+
+export default {
+  layout: "admin",
+  components: { vSelect },
+  data() {
+    return {
+      customToolbar: [
+        ["bold", "italic", "underline"],
+        [{ list: "ordered" }, { list: "bullet" }]
+      ],
+      loading: false,
+      selectedId: this.$route.params.id,
+      dataDetail: {},
+      form: {
+        nama_singkat: "",
+        nama_panjang: "",
+        jenis: "",
+        deskripsi: "",
+        saran: "",
+        profesi: "",
+        partner: ""
+      }
+    };
+  },
+  mounted() {
+    if (!this.$route.params.id)
+      return this.$router.push("/administrator/mbti/personality");
+    this.getDetail("kepribadian", this.$route.params.id);
+  },
+  methods: {
+    validateForm() {
+      console.log(this.form);
+      if (
+        !this.form.nama_singkat ||
+        !this.form.nama_panjang ||
+        !this.form.jenis ||
+        !this.form.deskripsi ||
+        !this.form.saran ||
+        !this.form.profesi ||
+        !this.form.partner
+      ) {
+        this.$bvToast.toast("Mohon lengkapi form dengan benar!", {
+          title: "Peringatan",
+          variant: "warning",
+          solid: true,
+          autoHideDelay: 2000
+        });
+        return;
+      }
+      this.submitData("dimensi");
+    },
+    submitData(type) {
+      this.loading = true;
+      this.$axios
+        .$put(`/api/${type}/create`, this.form)
+        .then(res => {
+          console.log(res);
+          if (res.success) {
+            this.$root.$bvToast.toast(
+              "Data " + type + " berhasil diperbarui.",
+              {
+                title: "Sukses",
+                variant: "success",
+                solid: true,
+                autoHideDelay: 3000
+              }
+            );
+            this.$router.replace("/administrator/mbti/personality");
+          }
+          return true;
+        })
+        .catch(err => {
+          console.log(err);
+          this.catchError(err);
+        })
+        .finally(() => (this.loading = false));
+    },
+    getDetail(type, id) {
+      this.loading = true;
+      this.$axios
+        .$get(`/api/${type}/find/${id}`)
+        .then(res => {
+          console.log(res);
+          if (res.success) {
+            this.dataDetail = res.data;
+            this.form = { ...this.dataDetail };
+          }
+          return true;
+        })
+        .catch(err => {
+          console.log(err);
+          this.catchError(err);
+        })
+        .finally(() => (this.loading = false));
+    }
+  }
+};
+</script>
---
+++ pages/administrator/mbti/personality/index.vue
@@ -0,0 +1,246 @@
+<template>
+  <div class="container-fluid crud">
+    <div class="row d-flex no-gutters">
+      <div class="col-md-12 dashboard px-0">
+        <h2 class="dash-label">Kepribadian</h2>
+        <p>
+          Data ini berisi data kepribadian dari dimensi sifat dasar manusia.
+          Data ini merupakan data master yang dibutuhkan untuk transaksi data
+          modul MBTI.
+        </p>
+      </div>
+      <div class="col-md-12 text-right mt-4 crud-tools">
+        <div class="row no-gutters justify-content-between">
+          <div class="col-md-3">
+            <b-input-group>
+              <template #prepend>
+                <b-input-group-text
+                  ><i class="fas fa-filter"></i
+                ></b-input-group-text>
+              </template>
+              <b-form-select
+                v-model="filter.perPage"
+                :options="[
+                  { text: 'Tampil 5', value: 5 },
+                  { text: 'Tampil 10', value: 10 },
+                  { text: 'Tampil 25', value: 25 },
+                  { text: 'Tampil 50', value: 50 }
+                ]"
+              ></b-form-select>
+            </b-input-group>
+          </div>
+
+          <div class="col-md-5 d-flex align-items-center crud-tools">
+            <b-input-group>
+              <template #prepend>
+                <b-input-group-text
+                  ><i class="fas fa-search"></i
+                ></b-input-group-text>
+              </template>
+              <b-form-input
+                v-model="filter.keyword"
+                placeholder="Cari kepribadian"
+                debounce="1000"
+              ></b-form-input>
+            </b-input-group>
+            <!-- <nuxt-link
+              class="btn btn-primary tambah crud-btn__add px-4 ml-2"
+              to="/administrator/mbti/kepribadian/add"
+            >
+              Tambah
+            </nuxt-link> -->
+          </div>
+        </div>
+      </div>
+      <div class="col-md-12 crud-body">
+        <div class="overflow-auto">
+          <table class="table-data-kelas table table-borderless">
+            <thead class="thead-table">
+              <tr>
+                <th class="no" width="100px">No</th>
+                <th>Kepribadian</th>
+                <th>Jenis Kepribadian</th>
+                <th>Partner Alami</th>
+                <th width="100" class="aksi">Aksi</th>
+              </tr>
+            </thead>
+            <tbody class="body-table">
+              <template v-if="totalRows > 0">
+                <tr v-for="(item, index) in items" :key="index">
+                  <td class="text-center">{{ index + 1 }}</td>
+                  <td>{{ item.nama_singkat }} ({{ item.nama_panjang }})</td>
+                  <td>{{ item.jenis }}</td>
+                  <td>{{ item.partner }}</td>
+                  <td class="btn-table">
+                    <nuxt-link
+                      class="btn btn-light px-2"
+                      :to="`/administrator/mbti/personality/edit/${item.id}`"
+                    >
+                      <i class="fa fa-edit"></i>
+                    </nuxt-link>
+                    <!-- <button
+                      class="btn btn-light px-2"
+                      @click.prevent="
+                        selectedId = item.id;
+                        selectedIndex = index;
+                        $bvModal.show('modal-delete');
+                      "
+                    >
+                      <i class="fa fa-trash"></i>
+                    </button> -->
+                  </td>
+                </tr>
+              </template>
+              <UITableLoading tag="div" v-if="loading" />
+              <UITableNotFound
+                tag="div"
+                v-if="totalRows == 0 && filter.keyword && !loading"
+              />
+            </tbody>
+          </table>
+
+          <b-pagination
+            class="pagination-table"
+            v-if="totalRows > 0 && totalRows > filter.perPage && !loading"
+            v-model="filter.page"
+            :total-rows="totalRows"
+            :per-page="filter.perPage"
+          >
+          </b-pagination>
+        </div>
+      </div>
+
+      <div
+        v-if="totalRows == 0 && !loading && !filter.keyword"
+        class="crud-body kosong"
+      >
+        <h2 class="kosong-title">Oops!</h2>
+        <p class="kosong-subtitle">Belum ada kepribadian</p>
+        <nuxt-link
+          class="btn btn-primary tambah-kelas px-4 py-3"
+          to="/administrator/mbti/personality/add"
+        >
+          Tambah kepribadian
+        </nuxt-link>
+      </div>
+    </div>
+
+    <!-- <b-modal
+      id="modal-delete"
+      title="Konfirmasi Hapus kepribadian"
+      hide-footer
+      centered
+      modal-class="admin-modal"
+      @hidden="resetModal"
+    >
+      <div>
+        <p class="modal-text">
+          Apakah anda yakin ingin menghapus data kepribadian ini?
+        </p>
+        <div class="modal-footer justify-content-end" style="border: 0px">
+          <button
+            class="btn btn-outline-secondary"
+            type="button"
+            @click="$bvModal.hide('modal-delete')"
+          >
+            Tidak
+          </button>
+          <button
+            class="btn btn-primary tambah px-4 py-2"
+            type="button"
+            :disabled="loading"
+            @click.prevent="deleteData('kepribadian')"
+          >
+            <b-spinner small v-if="loading" class="mr-1"></b-spinner> Ya
+          </button>
+        </div>
+      </div>
+    </b-modal> -->
+  </div>
+</template>
+
+<script>
+export default {
+  layout: "admin",
+  data() {
+    return {
+      loading: false,
+      filter: {
+        page: 1,
+        perPage: 5,
+        keyword: ""
+      },
+      totalRows: 0,
+      items: [],
+      selectedId: null,
+      selectedIndex: null
+    };
+  },
+  created() {
+    this.getData("kepribadian");
+  },
+  watch: {
+    "filter.keyword": function(value) {
+      this.getData("kepribadian");
+    },
+    "filter.page": function(value) {
+      this.getData("kepribadian");
+    },
+    "filter.perPage": function(value) {
+      this.getData("kepribadian");
+    }
+  },
+  methods: {
+    resetModal() {},
+    getData(type) {
+      this.loading = true;
+      this.$axios
+        .$get(`/api/${type}`, {
+          params: {
+            q: this.filter.keyword,
+            paginate: this.filter.perPage,
+            page: this.filter.page
+          }
+        })
+        .then(res => {
+          console.log(res);
+          if (res.success) {
+            this.items = res.data.data;
+            this.totalRows = res.data.total;
+            this.filter.perPage = res.data.per_page;
+          }
+          return true;
+        })
+        .catch(err => {
+          console.log(err);
+          this.catchError(err);
+        })
+        .finally(() => (this.loading = false));
+    },
+    deleteData(type) {
+      this.loading = true;
+      this.$axios
+        .$delete(`/api/${type}/delete/${this.selectedId}`)
+        .then(res => {
+          console.log(res);
+          if (res.success) {
+            this.items.splice(this.selectedIndex, 1);
+            this.$bvToast.toast("Data " + type + " berhasil dihapus.", {
+              title: "Sukses",
+              variant: "success",
+              solid: true,
+              autoHideDelay: 3000
+            });
+            this.$bvModal.hide("modal-delete");
+          }
+          return true;
+        })
+        .catch(err => {
+          console.log(err);
+          this.catchError(err);
+        })
+        .finally(() => (this.loading = false));
+    }
+  }
+};
+</script>
