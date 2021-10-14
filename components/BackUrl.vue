4284b6529cffc19b5118383070609c8b83b9c58d
Arbi Syarifudin <arbisyarifudin@gmail.com>
2021-10-14 20:57:25 +0700

    fixes bug wont click

--- components/BackUrl.vue
+++ components/BackUrl.vue
@@ -1,15 +1,24 @@
 <template>
   <router-link
+    v-if="url"
     role="button"
-    :to="url ? url : ''"
-    :tag="url ? 'a' : 'button'"
-    @click.prevent="url ? '' : $router.go(-1)"
+    :to="url"
     class="btn btn-outline-secondary mr-2 square"
     :title="title"
   >
     <b-icon icon="arrow-left" class="mr-1"></b-icon>
     <span v-if="title">{{ title }}</span>
   </router-link>
+  <button
+    v-else
+    role="button"
+    @click.prevent="$router.go(-1)"
+    class="btn btn-outline-secondary mr-2 square"
+    :title="title"
+  >
+    <b-icon icon="arrow-left" class="mr-1"></b-icon>
+    <span v-if="title">{{ title }}</span>
+  </button>
 </template>
 
 <script>
