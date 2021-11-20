bab7972d803e86c50d72f7c78625c291d4712ca6
Arbi Syarifudin <arbisyarifudin@gmail.com>
2021-11-20 18:39:01 +0700

    Create Kelas Kursus CRUD (WIP)

--- pages/app/partner/courses/index.vue
+++ pages/app/partner/courses/index.vue
@@ -4,10 +4,10 @@
     <div class="row d-flex no-gutters">
       <div class="col-md-12 dashboard px-0">
         <h2 class="dash-label">
-          Kelas Kursus Saya
+          Kelas Privat Saya
         </h2>
         <p>
-          Ini adalah daftar kelas kursus yang Anda buat.
+          Semua kelas privat buatanmu ada disini. Ayo terus buat kelasmu dan raih pendapatan lebih dengan mengajar para siswa-siswa.
         </p>
       </div>
       <div class="col-md-12 text-right mt-4 crud-tools">
@@ -22,30 +22,14 @@
                     ></b-input-group-text>
                   </template>
                   <b-form-select
-                    v-model="filterCategory"
+                    v-model="filter.perPage"
                     :options="[
-                      { text: 'UTBK Mandiri', value: 'UTBK Mandiri' },
-                      { text: 'UTBK Akbar', value: 'UTBK Akbar' },
-                      { text: 'ASPD', value: 'ASPD' },
-                      { text: 'Paket Tryout', value: 'Paket Tryout' }
+                      { text: 'Tampil 6', value: 6 },
+                      { text: 'Tampil 24', value: 24 },
+                      { text: 'Tampil 64', value: 64 },
+                      { text: 'Tampil 100', value: 100 },
                     ]"
-                  ></b-form-select>
-                </b-input-group>
-              </div>
-              <div class="col-md-5">
-                <b-input-group>
-                  <template #prepend>
-                    <b-input-group-text
-                      ><i class="fas fa-filter"></i
-                    ></b-input-group-text>
-                  </template>
-                  <b-form-select
-                    v-model="filterStatus"
-                    :options="[
-                      { text: 'Semua', value: null },
-                      { text: 'Sudah Dikerjakan', value: 'Sudah Dikerjakan' },
-                      { text: 'Belum Dikerjakan', value: 'Belum Dikerjakan' }
-                    ]"
+                    @change="getData"
                   ></b-form-select>
                 </b-input-group>
               </div>
@@ -61,10 +45,16 @@
               </template>
               <b-form-input
                 v-model="filter.keyword"
-                placeholder="Cari Tryout"
+                placeholder="Cari Kelas"
                 debounce="1000"
               ></b-form-input>
             </b-input-group>
+             <nuxt-link
+              class="btn btn-primary tambah crud-btn__add px-4 ml-2"
+              to="/app/partner/courses/add"
+            >
+              Tambah
+            </nuxt-link>
           </div>
         </div>
       </div>
@@ -78,102 +68,51 @@
             <div class="card card-karir m-2">
               <!-- style="width: 350px; max-width: 100%" -->
               <div class="card-body text-left p-0">
-                <!-- <div class="" style="display: flex; justify-content: flex-end;">
+                <div class="" style="display: flex; justify-content: flex-end;">
                   <p
                     class="label-event mb-2 px-4 py-1"
-                    :class="[item.status_produk == 'Aktif' ? '' : 'draft']"
+                    :class="[item.menerima_peserta == 'Aktif' ? '' : 'draft']"
                   >
-                    {{ item.status_produk == "Aktif" ? "Publish" : "Draft" }}
+                    {{ item.menerima_peserta == "Aktif" ? "Aktif" : "Nonaktif" }}
                   </p>
-                </div> -->
-                <div class="card-content px-4 mt-3">
+                </div>
+                <div class="card-content px-4 mt-1">
+                   <p class="mb-2" style="font-size: 14px">
+                    <i class="far fa-star fa-fw"></i>
+                    <i class="far fa-star fa-fw"></i>
+                    <i class="far fa-star fa-fw"></i>
+                    <i class="far fa-star fa-fw"></i>
+                    <i class="far fa-star fa-fw"></i>
+                  </p>
                   <h3
                     class="card-judul card-program mb-4 mt-3"
                     style="height: 60px"
                   >
-                    {{ item.nama_produk }}
+                    {{ item.nama_kursus }}
                   </h3>
                   <!-- <h5>TPS</h5> -->
-                  <div
-                    class="d-flex justify-content-between mb-2"
-                    v-if="item.tipe_paket == 'Reguler'"
-                  >
-                    <p class="mb-2">
-                      <i class="far fa-clock fa-fw"></i> {{ item.waktu }} Menit
-                    </p>
-                    <p class="mb-2">
-                      <i class="far fa-file-alt fa-fw"></i>
-                      {{ item.jumlah_soal }} Soal
-                    </p>
-                  </div>
+                  <p class="mb-2">
+                    <i class="fas fa-award fa-fw"></i> {{item.mapel ? item.mapel.nama_mapel : '-'}}
+                  </p>
                   <div class="d-flex justify-content-between mb-2">
                     <p class="">
-                      <i class="fas fa-tags fa-fw"></i>
-                      {{ item.harga_label }}
+                      <i class="fas fa-tags fa-fw"></i> Rp 
+                      {{ formatRupiah(item.harga_kursus) }}
                     </p>
-                    <p
-                      class=""
-                      v-if="item.jenis_produk == 'Masal' || item.tanggal_mulai"
-                    >
-                      <i class="fas fa-calendar-alt fa-fw"></i>
-                      {{ item.tanggal_mulai_label }}
-                    </p>
-                    <p v-if="item.tipe_paket == 'Bundling'">
-                      {{ item.tanggal_mulai_label }}
-                    </p>
-                    <p
-                      class="beda"
-                      v-if="
-                        item.kategori_produk == 'ASPD' ||
-                          item.kategori_produk == 'Asmenas'
-                      "
-                    >
-                      {{ item.penjurusan[0] }}
+                    <p class="beda">
+                     {{item.jenjang ? item.jenjang.nama_jenjang : '-'}}
                     </p>
                   </div>
-                  <div
-                    v-if="item.tipe_paket == 'Bundling'"
-                    class="package-getlist mb-3"
-                    v-html="item.deskripsi_produk"
-                  ></div>
                 </div>
                 <div
                   class="card-bawah pb-3 px-4 m-0 bordered"
                   style="border-bottom: 12px solid #D7D2F7; border-radius:0px 0px 12px 12px;"
                 >
                   <nuxt-link
-                    :to="`/app/tryout/${item.id}/detail?ref=${$route.path}`"
+                    :to="`/app/partner/courses/${item.id}/detail?ref=${$route.path}`"
                     class="karir-link"
                     >Detail <i class="fas fa-chevron-right ml-1"></i
                   ></nuxt-link>
-                  <div class="icon-footer center">
-                    <h4 class="title">TRYOUT</h4>
-                    <h5 class="subtitle">
-                      <span class="mr-0">{{ item.kategori_produk }}</span
-                      ><span class="ml-0" v-if="item.jenis_tryout[0]">
-                        - {{ item.jenis_tryout[0] }}</span
-                      ><span
-                        class="ml-0"
-                        v-if="
-                          item.kategori_produk == 'UTBK' &&
-                            item.kelompok_soal[0]
-                        "
-                      >
-                        {{ item.kelompok_soal[0] }}</span
-                      >
-                      <span
-                        class="ml-0"
-                        v-if="
-                          item.kategori_produk == 'ASPD' ||
-                            item.kategori_produk == 'Asmenas'
-                        "
-                      >
-                        - {{ item.penjurusan[0] }}</span
-                      >
-                    </h5>
-                    <!-- <div>2021</div> -->
-                    <img src="/icon/icon-card-bg.png" class="img-fluid image" />
-                  </div>
                 </div>
               </div>
             </div>
@@ -225,14 +164,9 @@ export default {
         page: 1,
         perPage: 6,
         keyword: "",
-        category: "UTBK",
-        package: "Reguler",
-        event: "Perorangan"
       },
       totalRows: 0,
       items: [],
-      filterCategory: "UTBK Mandiri",
-      filterStatus: null
     };
   },
   computed: {
@@ -245,67 +179,30 @@ export default {
   },
   watch: {
     "filter.keyword": function(value) {
-      this.getData("produk");
+      this.getData();
     },
     "filter.page": function(value) {
-      this.getData("produk");
+      this.getData();
     },
-    filterCategory: function(value) {
-      this.getData("produk");
-    }
   },
   created() {
-    this.getData("produk");
+    this.getData();
   },
   methods: {
-    getData(type) {
-      switch (this.filterCategory) {
-        case "UTBK Mandiri":
-          this.filter.category = "UTBK";
-          this.filter.event = "Perorangan";
-          this.filter.package = "Reguler";
-          break;
-        case "UTBK Akbar":
-          this.filter.category = "UTBK";
-          this.filter.event = "Masal";
-          this.filter.package = "Reguler";
-          break;
-        case "ASPD":
-          this.filter.category = "ASPD";
-          this.filter.event = "Perorangan";
-          this.filter.package = "Reguler";
-          break;
-        case "Paket Tryout":
-          this.filter.category = "";
-          this.filter.event = "";
-          this.filter.package = "Bundling";
-          break;
-        default:
-          this.filter.category = "UTBK";
-          this.filter.event = "Perorangan";
-          this.filter.package = "Reguler";
-          break;
-      }
+    getData() {
       this.loading = true;
       this.$axios
-        .$get(`/api/${type}`, {
+        .$get('api/kursus', {
           params: {
             q: this.filter.keyword,
             paginate: this.filter.perPage,
-            page: this.filter.page,
-            kategori_produk: this.filter.category,
-            jenis_produk: this.filter.event,
-            status_produk: "Aktif",
-            tipe_paket: this.filter.package,
-            id_user: this.user.id
           }
         })
         .then(res => {
-          console.log(res);
           if (res.success) {
             this.items = res.data.data;
-            this.totalRows = res.data.paginate.total;
-            this.filter.perPage = res.data.paginate.per_page;
+            this.totalRows = res.data.total;
+            this.filter.perPage = res.data.per_page;
           }
           return true;
         })
