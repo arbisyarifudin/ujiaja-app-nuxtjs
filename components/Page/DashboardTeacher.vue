5b138d40a1297106fe8da6ad4706ba5fac0cdaf4
Arbi Syarifudin <arbisyarifudin@gmail.com>
2021-11-30 00:50:58 +0700

    Split dashboard component by user role (teacher & student)

---
+++ components/Page/DashboardTeacher.vue
@@ -0,0 +1,23 @@
+<template>
+   <div class="container-fluid konten-editprofil">
+    <div class="col-md-12 dash-kelas p-0 text-left">
+      <h2 class="pb-0">Dashboard</h2>
+      <h5 class="mt-4">
+        Halo {{ userDetail.nama_lengkap }}, selamat datang!<i class=""></i>
+      </h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    user() {
+      return this.$store.state.dataUser.user;
+    },
+    userDetail() {
+      return this.$store.state.dataUser.detail;
+    }
+  }
+};
+</script>
