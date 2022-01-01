0f76bb393dfa9a42b6fe0b47a1ef0ac861a7abb3
Arbi Syarifudin <arbisyarifudin@gmail.com>
2022-01-01 21:29:19 +0700

    minor fixes in sectionTryoutAkabar

--- components/Section/SectionProgramStudi.vue
+++ components/Section/SectionProgramStudi.vue
@@ -2,12 +2,27 @@
   <div class="section section-prodi">
     <div class="tryout-akbar no-bg pb-3">
       <div class="col-md-12 mt-3 hero-lagi tryout text-center">
-        <h2 class="m-0 pb-3">Bingung harus mulai dari mana agar dapat progam studi yang sesuai denganmu?</h2>
-        <p class="p-0">Buat kamu yang ingin kuliah di program studi yang sesuai dengan kamu banget, yuk cari disini bareng UjiAja.</p>
-        <div class="col-md-5 mx-auto my-5 brandaplus"><img class="mt-5 mb-5" src="/icon/brandatambahan.png" alt=""></div>
-        <h4>Pastikan progam studi yang kamu pilih sesuai dengan minat dan kemampuan kamu.</h4>
-        <p class="p-0">Ayo, temukan sekarang juga progam studi yang cocok denganmu.</p>
-        <a class="btn btn-outline-primary mt-5" href="/cari-program-studi">Temukan Sekarang</a>
+        <h2 class="m-0 pb-3">
+          Bingung harus mulai dari mana agar dapat progam studi yang sesuai
+          denganmu?
+        </h2>
+        <p class="p-0">
+          Buat kamu yang ingin kuliah di program studi yang sesuai dengan kamu
+          banget, yuk cari disini bareng UjiAja.
+        </p>
+        <div class="col-md-5 mx-auto my-5 brandaplus">
+          <img class="mt-5 mb-5" src="/icon/brandatambahan.png" alt="" />
+        </div>
+        <h4>
+          Pastikan progam studi yang kamu pilih sesuai dengan minat dan
+          kemampuan kamu.
+        </h4>
+        <p class="p-0">
+          Ayo, temukan sekarang juga progam studi yang cocok denganmu.
+        </p>
+        <a class="btn btn-outline-primary mt-5" href="/cari-program-studi"
+          >Temukan Sekarang</a
+        >
       </div>
     </div>
   </div>
--- components/Section/SectionTryoutAkbar.vue
+++ components/Section/SectionTryoutAkbar.vue
@@ -3,11 +3,21 @@
     <div class="tryout-akbar pb-3">
       <div class="col-md-12 hero-lagi tryout text-center">
         <h2 class="m-0 pb-3">Try Out Akbar</h2>
-        <p class="p-0">Yuk, uji kemampuanmu dengan mengikuti Try Out Akbar UTBK, Asesmen Nasional, ASPD dan UKTT 2021!</p>
-        <img class="mt-5 mb-5" src="/icon/tryoutakbar.png" alt="">
-        <h4>Persiapkan Tryout yang akan kamu ikuti bersama Tryou Akbar di UjiAja.</h4>
-        <p class="p-0">Tryout berbasis sistem CAT sesuai kisi-kisi 2021. Ayo, ikuti Tryout Akbar sekarang juga.</p>
-        <add class="btn btn-outline-primary mt-5" href="/registrasi">Daftar Sekarang</add>
+        <p class="p-0">
+          Yuk, uji kemampuanmu dengan mengikuti Try Out Akbar UTBK, Asesmen
+          Nasional, ASPD dan UKTT 2021!
+        </p>
+        <img class="mt-5 mb-5" src="/icon/tryoutakbar.png" alt="" />
+        <h4>
+          Persiapkan Tryout yang akan kamu ikuti bersama Tryou Akbar di UjiAja.
+        </h4>
+        <p class="p-0">
+          Tryout berbasis sistem CAT sesuai kisi-kisi 2021. Ayo, ikuti Tryout
+          Akbar sekarang juga.
+        </p>
+        <a class="btn btn-outline-primary mt-5" href="/registrasi"
+          >Daftar Sekarang</a
+        >
       </div>
     </div>
   </div>
