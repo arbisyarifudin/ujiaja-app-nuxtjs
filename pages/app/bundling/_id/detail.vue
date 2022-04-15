91baa9754384593af93964d69cedab1fd5bd5f3a
Arbi Syarifudin <arbisyarifudin@gmail.com>
2022-04-15 11:38:52 +0700

    change product list in bundling detail to card mode

--- pages/app/bundling/_id/detail.vue
+++ pages/app/bundling/_id/detail.vue
@@ -77,7 +77,135 @@
         <div class="card card-header bg-white pt-4 pb-3">
           <h3 class="h4 mb-0">Daftar Produk dari Bundling Ini</h3>
         </div>
-        <UILoading v-if="loading" />
+        <div class="card-body bg-white">
+          <div class="row">
+            <div
+              class="col-xl-4 col-lg-6 col-md-6 col-sm-6"
+              v-for="(item, i) in dataDetail.products"
+              :key="i"
+            >
+              <div class="card card-karir m-2 router-push" @click="$router.push(`/app/tryout/${item.id}/detail?ref=${$route.path}`)">
+                <!-- style="width: 350px; max-width: 100%" -->
+                <div class="card-body text-left p-0">
+                  <div class="" style="display: flex; justify-content: flex-end;">
+                  <p
+                    class="label-event mb-2 px-4 py-1"
+                    :class="[item.transaksi_user_product_bundling &&
+                      item.transaksi_user_product_bundling.status != 'Ditolak' &&
+                      item.transaksi_user_product_bundling.status != 'Kadaluarsa' ? '' : 'draft']"
+                  >
+                    {{ item.transaksi_user_product_bundling &&
+                      item.transaksi_user_product_bundling.status != 'Ditolak' &&
+                      item.transaksi_user_product_bundling.status != 'Kadaluarsa' ? "Sudah Dibeli" : "Belum Dibeli" }}
+                  </p>
+                </div>
+                  <div class="card-content px-4">
+                  <h3
+                    class="card-judul card-program mb-4 mt-3"
+                    style="height: 60px"
+                  >
+                    {{ item.nama_produk }}
+                  </h3>
+                  <!-- <h5>TPS</h5> -->
+                  <div
+                    class="d-flex justify-content-between mb-2"
+                    v-if="item.tipe_paket == 'Reguler'"
+                  >
+                    <p class="mb-2">
+                      <i class="far fa-clock fa-fw"></i> {{ item.waktu }} Menit
+                    </p>
+                    <p class="mb-2">
+                      <i class="far fa-file-alt fa-fw"></i>
+                      {{ item.jumlah_soal }} Soal
+                    </p>
+                  </div>
+                  <div class="d-flex justify-content-between mb-2">
+                    <p class="">
+                      <i class="fas fa-tags fa-fw"></i>
+                      {{ item.harga_produk > 0 ? item.harga_label : "GRATIS" }}
+                    </p>
+                    <p v-if="item.jenis_produk == 'Masal'">
+                      {{ item.tanggal_mulai_label }}
+                    </p>
+                    <p
+                      class="beda"
+                      v-if="
+                        item.kategori_produk == 'ASPD' ||
+                          item.kategori_produk == 'Asmenas'
+                      "
+                    >
+                      {{ item.penjurusan[0] }}
+                    </p>
+                    <p
+                      class="beda"
+                      v-if="
+                        item.kategori_produk == 'UTBK' &&
+                          item.tipe_paket == 'Bundling'
+                      "
+                    >
+                      Bundling
+                    </p>
+                  </div>
+                  <div
+                    v-if="item.tipe_paket == 'Bundling'"
+                    class="package-getlist mb-3"
+                    v-html="item.deskripsi_produk"
+                  ></div>
+                </div>
+                <div
+                  class="card-bawah pb-3 px-4 m-0 bordered"
+                  style="border-bottom: 12px solid #D7D2F7; border-radius:0px 0px 12px 12px;"
+                >
+                  <nuxt-link
+                    :to="`/administrator/product/${item.id}/detail`"
+                    class="karir-link"
+                    >Detail <i class="fas fa-chevron-right ml-1"></i
+                  ></nuxt-link>
+                  <div class="icon-footer center">
+                    <h4 class="title">TRYOUT</h4>
+                    <h5 class="subtitle">
+                      <span class="mr-0">{{ item.kategori_produk }}</span
+                      ><span
+                        class="ml-0"
+                        v-if="
+                          item.jenis_tryout[0] && item.tipe_paket !== 'Bundling'
+                        "
+                      >
+                        - {{ item.jenis_tryout[0] }}</span
+                      ><span
+                        class="ml-0"
+                        v-if="
+                          item.kategori_produk == 'UTBK' &&
+                            item.kelompok_soal[0]
+                        "
+                      >
+                        {{ item.kelompok_soal[0] }}</span
+                      >
+                      <span
+                        class="ml-0"
+                        v-if="
+                          item.kategori_produk == 'ASPD' ||
+                            item.kategori_produk == 'Asmenas'
+                        "
+                      >
+                        - {{ item.penjurusan[0] }}</span
+                      >
+                    </h5>
+                    <!-- <div>2021</div> -->
+                    <img src="/icon/icon-card-bg.png" class="img-fluid image" />
+                  </div>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <UILoading
+            tag="div"
+            style="position: static; width: 100%;"
+            v-if="loading"
+          />
+        </div>
+        <!-- <UILoading v-if="loading" />
         <div
           v-else
           class="bg-white p-5"
@@ -118,7 +246,7 @@
               {{ formatTanggal(bp.product.tanggal_berakhir) }}
             </li>
           </ul>
-        </div>
+        </div> -->
       </div>
     </div>
   </div>
