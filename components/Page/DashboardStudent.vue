5d5dab7a6eeb490e1c489539a0b3cf9e8c5456e3
Arbi Syarifudin <arbisyarifudin@gmail.com>
2022-04-15 13:08:10 +0700

    add ASPD Battery in DashboardStudent component

--- components/Page/DashboardStudent.vue
+++ components/Page/DashboardStudent.vue
@@ -2,32 +2,94 @@
   <div class="container-fluid konten-editprofil">
     <div class="col-md-12 dash-kelas p-0 text-left pos-relative">
       <h2 class="pb-0">Dashboard</h2>
-      <UILoading v-if="loading"/>
-      <h5 class="mt-2 mb-3">Halo {{userDetail.nama_lengkap}}, selamat datang!</h5>
-      <h5 class="mb-4" v-if="isDisplayBatteries">
-        Yuk terus belajar, dan naikan score UTBK kamu untuk meningkatkan peluang masuk
-        ke Perguruan Tinggi impianmu.
+      <UILoading v-if="loading" />
+      <h5 class="mt-2 mb-3">
+        Halo {{ userDetail.nama_lengkap }}, selamat datang!
       </h5>
       <div class="row justify-content-around" v-if="isDisplayBatteries">
-        <div class="col-md-3 col-6 text-center">
-          <div class="h5">{{prodiSatu.program_studi ? prodiSatu.program_studi.nama_studi : '-'}}</div>
-          <div class="h6">Passing Grade {{prodiSatu.passing_grade_prodi}}</div>
-          <div class="battery my-3">
-            <div class="battery-outline">
-              <div class="battery-fill" :style="{height: (190 * (scoreData.pg1_percent / 100)) + 'px'}">{{scoreData.pg1_percent}}%</div>
-            </div>
-          </div>
-          <div class="h6">{{prodiSatu.perguruan ? prodiSatu.perguruan.nama_perguruan : '-'}}</div>
+        <div class="col-12">
+          <h4><i class="fas fa-fw fa-award mr-1"></i> PERSENTASE NILAI UTBK</h4>
         </div>
-         <div class="col-md-3 col-6 text-center">
-          <div class="h5">{{prodiDua.program_studi ? prodiDua.program_studi.nama_studi : '-'}}</div>
-          <div class="h6">Passing Grade {{prodiDua.passing_grade_prodi}}</div>
+        <div class="col-12">
+          <p class="mb-4 small">
+            Yuk terus belajar, dan naikan score UTBK kamu untuk meningkatkan
+            peluang masuk ke Perguruan Tinggi impianmu.
+          </p>
+        </div>
+        <div class="col-md-3 col-6 text-center">
+          <div class="h5">
+            {{
+              prodiSatu.program_studi ? prodiSatu.program_studi.nama_studi : "-"
+            }}
+          </div>
+          <div class="h6">
+            Passing Grade {{ prodiSatu.passing_grade_prodi }}
+          </div>
           <div class="battery my-3">
             <div class="battery-outline">
-              <div class="battery-fill" :style="{height: (190 * (scoreData.pg2_percent / 100)) + 'px'}">{{scoreData.pg2_percent}}%</div>
+              <div
+                class="battery-fill"
+                :style="{ height: 190 * (scoreData.pg1_percent / 100) + 'px' }"
+              >
+                {{ scoreData.pg1_percent }}%
+              </div>
             </div>
           </div>
-          <div class="h6">{{prodiDua.perguruan ? prodiDua.perguruan.nama_perguruan : '-'}}</div>
+          <div class="h6">
+            {{ prodiSatu.perguruan ? prodiSatu.perguruan.nama_perguruan : "-" }}
+          </div>
+        </div>
+        <div class="col-md-3 col-6 text-center">
+          <div class="h5">
+            {{
+              prodiDua.program_studi ? prodiDua.program_studi.nama_studi : "-"
+            }}
+          </div>
+          <div class="h6">Passing Grade {{ prodiDua.passing_grade_prodi }}</div>
+          <div class="battery my-3">
+            <div class="battery-outline">
+              <div
+                class="battery-fill"
+                :style="{ height: 190 * (scoreData.pg2_percent / 100) + 'px' }"
+              >
+                {{ scoreData.pg2_percent }}%
+              </div>
+            </div>
+          </div>
+          <div class="h6">
+            {{ prodiDua.perguruan ? prodiDua.perguruan.nama_perguruan : "-" }}
+          </div>
+        </div>
+      </div>
+      <div class="row justify-content-around" v-if="isDisplayASPDBatteries">
+        <div class="col-12">
+          <h4><i class="fas fa-fw fa-award mr-1"></i> PERSENTASE NILAI ASPD</h4>
+        </div>
+        <div class="col-12">
+          <p class="mb-4 small">
+            Yuk terus belajar, dan naikan score ASPD kamu untuk meningkatkan
+            peluang masuk ke sekolah di Jenjang selanjutnya.
+          </p>
+        </div>
+        <div class="col-md-3 col-6 text-center">
+          <div class="h6">
+            Nilai Sempurna ASPD {{ userDetail.nama_jenjang }} :
+            {{ scoreASPDData.perpect_score }}
+          </div>
+          <div class="battery my-3">
+            <div class="battery-outline">
+              <div
+                class="battery-fill"
+                :class="bgColorClass(scoreASPDData.percent_score)"
+                :style="{
+                  height: 190 * (scoreASPDData.percent_score / 100) + 'px'
+                }"
+              >
+                {{ scoreASPDData.percent_score }}%
+              </div>
+            </div>
+          </div>
+          <div class="h6">Nilai Rata-rata Kamu : {{ scoreASPDData.avg_score }}</div>
         </div>
       </div>
       <!-- <h5 class="mt-4">
@@ -49,7 +111,10 @@
       </h5>
       <div class="row">
         <div class="col-md-6">
-          <div class="card card-karir m-3 router-push" @click="$router.push('/app/tryout')">
+          <div
+            class="card card-karir m-3 router-push"
+            @click="$router.push('/app/tryout')"
+          >
             <div class="card-body text-left p-0">
               <div class="card-content px-4">
                 <h3
@@ -84,7 +149,10 @@
         </div>
         <div class="col-md-6">
           <div class="card card-karir m-3">
-            <div class="card-body text-left p-0 router-push" @click="$router.push('/app/tryout')">
+            <div
+              class="card-body text-left p-0 router-push"
+              @click="$router.push('/app/tryout')"
+            >
               <div class="card-content px-4">
                 <h3
                   class="card-judul card-program mt-4 mb-2"
@@ -173,24 +241,31 @@ export default {
     return {
       loading: true,
       isDisplayBatteries: false,
+      isDisplayASPDBatteries: false,
       scoreData: {},
+      scoreASPDData: {},
       prodiSatu: {},
       prodiDua: {}
     };
   },
   created() {
-    this.getProfilLengkap()
+    this.getProfilLengkap();
     this.getPersentaseSkor();
+    this.getPersentaseSkorASPD();
   },
   methods: {
     getPersentaseSkor() {
-      this.loading = true
+      this.loading = true;
       this.$axios
         .$get("/api/tryout_user/rerata-persentase-hasil", {
           params: {}
         })
         .then(response => {
-          if (response.success && this.userDetail.nama_jenjang == 'SMA') {
+          if (
+            response.success &&
+            (this.userDetail.nama_jenjang == "SMA" ||
+              ["SMA", "SMK", "MA"].includes(this.userDetail.nama_jenjang))
+          ) {
             this.scoreData = response.data;
             this.isDisplayBatteries = true;
           }
@@ -199,11 +274,30 @@ export default {
           this.isDisplayBatteries = false;
         })
         .finally(() => {
-          this.loading = false
+          this.loading = false;
+        });
+    },
+    getPersentaseSkorASPD() {
+      this.loading = true;
+      this.$axios
+        .$get("/api/tryout_user/rerata-persentase-hasil-aspd", {
+          params: {}
         })
+        .then(response => {
+          if (response.success) {
+            this.scoreASPDData = response.data;
+            this.isDisplayASPDBatteries = true;
+          }
+        })
+        .catch(error => {
+          this.isDisplayASPDBatteries = false;
+        })
+        .finally(() => {
+          this.loading = false;
+        });
     },
     getProfilLengkap() {
-      this.loading = true
+      this.loading = true;
       this.$axios
         .$get("/api/users/siswa/find/" + this.user.id, {
           params: {}
@@ -216,10 +310,21 @@ export default {
         })
         .catch(error => {
           this.isDisplayBatteries = false;
+          this.isDisplayASPDBatteries = false;
         })
         .finally(() => {
-          this.loading = false
-        })
+          this.loading = false;
+        });
+    },
+    bgColorClass(score) {
+      const _score = parseFloat(score)
+      if (_score >= 76) {
+        return 'bg-success'
+      } else if(_score >= 70) {
+        return 'bg-warning'
+      } else {
+        return 'bg-danger'
+      }
     }
   },
   computed: {
