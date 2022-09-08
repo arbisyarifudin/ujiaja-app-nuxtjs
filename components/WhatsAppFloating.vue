5beedf51d335af3185f86892eeb5399e1bde9195
Luthfy on Infinix <projectmanager@gmail.coom>
2022-09-08 09:17:30 +0800

    add floating whatsapp icon

---
+++ components/WhatsAppFloating.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <div class="floating">
+      <a href="https://api.whatsapp.com/send/?phone=6285773716731&text&type=phone_number&app_absent=0" target="_blank">
+        <img src="icon/WhatsApp.png" alt="">
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .floating {
+    position: fixed;
+    top: 85vh;
+    right: 0px;
+    margin: 3rem;
+    margin-bottom: 5rem;
+  }
+
+  .floating img {
+    width: 2.5rem;
+  }
+</style>
+
+<script>
+export default {
+  props: ["props"],
+};
+</script>
