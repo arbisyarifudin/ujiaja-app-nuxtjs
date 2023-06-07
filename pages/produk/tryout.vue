0958af27aaa08095065661d5eddae40b45ec8387
Angga Nurhiman <nurhimanangga@gmail.com>
2023-06-07 13:23:46 +0700

    feat(Tryout page): Section 3 API implementation

--- pages/produk/tryout.vue
+++ pages/produk/tryout.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <Header
-      variant="melengkung"
-      :heroData="header.hero"
-      :navData="navData"
-    />
+    <Header variant="melengkung" :heroData="header.hero" :navData="navData" />
     <SectionKeunggulan :props="keunggulan" />
     <SectionProduk :props="produk" />
     <SectionCardHero
@@ -32,7 +28,7 @@
 export default {
   head() {
     return {
-      title: "Tryout"
+      title: "Tryout",
     };
   },
   data() {
@@ -46,65 +42,25 @@ export default {
           ctaButtonVariant: "secondary",
           cataButtonUrl: "/registrasi",
           image: "/hero-tryout_3.png",
-          isJudulFirst: false
-        }
+          isJudulFirst: false,
+        },
       },
       keunggulan: {
         judul: "Mengapa Kamu harus mencoba tryout di UjiAja?",
-        item: []
+        item: [],
       },
       produk: {
         judul: "Ada berapa jenis tryout yang ada di UjiAja?",
         deskripsi:
           "UjiAja menyediakan semua amunisi yang kamu butuhkan untuk menuju kampus impianmu!<br /> Belajar efektif dengan rangkuman lengkap serta ratusan soal yang variatif dan menantang.",
-        item: [
-          {
-            gambar: "/produk/tryout1.png",
-            judul: 'Tryout <span class="large">UTBK</span>',
-            brand: "by UjiAja",
-            deskripsi:
-              "Persiapkan dirimu hadapi ujian UTBK dengan latihan Tryout yang akurat, lengkap dengan fitur terbaik.",
-            tambahan: "Gratis Tes Minat, Bakat & Potensi",
-            url: "/produk/tryout-utbk"
-          },
-          // {
-          //   gambar: "/produk/tryout2.png",
-          //   judul: 'Tryout <span class="large">Asesmen Nasional</span>',
-          //   brand: "by UjiAja",
-          //   deskripsi:
-          //     "Persiapkan dirimu hadapi ujian Asesmen Kompetensi Minimum (Literasi & Numerasi) dengan latihan Tryout AKM yang akurat. Tryout ini ditujukan untuk jenjang SD hingga SMA.",
-          //   tambahan: null,
-          //   url: "/produk/tryout-asesmen-nasional",
-          // },
-          {
-            gambar: "/produk/tryout3.png",
-            judul: 'Tryout <span class="large">ASPD</span>',
-            brand: "by UjiAja",
-            deskripsi:
-              "Persiapkan dirimu hadapi ujian Asesmen Standar Pendidikan Daerah (Khusus DIY) dengan latihan Tryout ASPD yang akurat. Tryout ini ditujukan untuk jenjang SD hingga SMP.",
-            tambahan: null,
-            url: "/produk/tryout-aspd"
-          },
-          {
-            gambar: "/produk/tryout3.png",
-            judul: 'Tryout <span class="large">PAT/PAS</span>',
-            brand: "by UjiAja",
-            deskripsi:
-              "Persiapkan dirimu hadapi ujian Kenaikan Kelas dengan latihan Tryout PAT dan PAS yang akurat. Tryout ini ditujukan untuk jenjang siswa-siswi Kelas 4,5, dan 6 SD ; Kelas 7,8, dan 9 SMP; serta 10,11, dan 12 SMA.",
-            tambahan: null,
-            url: "#"
-          },
-          {
-            gambar: "/produk/tryout4.jpg",
-            judul: 'MB<span class="large">TI</span>',
-            brand: "by UjiAja",
-            deskripsi:
-              "Persiapkan kuliah, dan potensimu dengan pilihan yang tepat sesuai minat dan bakat dengan ikut MBTI. MBTI ini ditujukan untuk jenjang SD hingga SMA.",
-            tambahan: null,
-            url: "/produk/tes-mbti"
-          }
-        ]
+        item: [],
       },
+      gambarProduk: [
+        "/produk/tryout1.png",
+        "/produk/tryout2.png",
+        "/produk/tryout3.png",
+        "/produk/tryout4.jpg",
+      ],
       testimoni: {
         judul: "Ratusan alumni UjiAja Berhasil mewujudkan kampus impian",
         deskripsi: "",
@@ -115,23 +71,23 @@ export default {
             titel: "Kedokteran - Universitas Negeri Semarang",
             testimoni:
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Adipiscing odio id condimentum nisl. Vulputate consequat purus amet facilisis est, lectus non urna morbi. Cursus posuere donec congue velit vitae sit massa purus. Adipiscing at euismod morbi nisl, sit et risus. Eget aliquam lacus vel turpis tellus sed nulla.",
-            foto: "/carla.png"
+            foto: "/carla.png",
           },
           {
             nama: "Wilson Aminoff",
             titel: "Ilmu Komputer - Universitas Gajah Mada",
             testimoni:
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fames sed nam eget aenean ultrices proin metus. Ut risus, sed dictumst viverra. Faucibus sed fermentum in eros mi in tellus. Varius consequat, amet ut nulla massa rhoncus. Nibh in adipiscing.",
-            foto: "/wilson.png"
-          }
-        ]
-      }
+            foto: "/wilson.png",
+          },
+        ],
+      },
     };
   },
   asyncData(context) {
     function getSetting(key) {
       const settings = context.store.state.dataSetting;
-      const foundSetting = settings.find(item => item.key == key);
+      const foundSetting = settings.find((item) => item.key == key);
       if (foundSetting) {
         return foundSetting.isi;
       }
@@ -139,7 +95,7 @@ export default {
     }
 
     const navData = {
-      logo: getSetting("logo")
+      logo: getSetting("logo"),
     };
 
     const footerData = {
@@ -150,19 +106,19 @@ export default {
       instagram: getSetting("instagram"),
       facebook: getSetting("facebook"),
       youtube: getSetting("youtube"),
-      email: getSetting("email")
+      email: getSetting("email"),
     };
 
     return {
       navData,
-      footerData
+      footerData,
     };
   },
   created() {
     // console.log(this.$store.getters['checkIsAuth'])
-    if(this.$store.getters['checkIsAuth']) {
-      this.header.hero.ctaButtonText = 'Pergi ke Dashboard'
-      this.header.hero.cataButtonUrl = '/app/dashboard'
+    if (this.$store.getters["checkIsAuth"]) {
+      this.header.hero.ctaButtonText = "Pergi ke Dashboard";
+      this.header.hero.cataButtonUrl = "/app/dashboard";
     }
   },
   async fetch() {
@@ -173,7 +129,7 @@ export default {
       if (resContent.data.success) {
         this.setHeroSection(res.data.dataContent1[0]);
         this.keunggulan.item = res.data.dataContent2;
-        // this.setProductSection(res.data.dataContent3);
+        this.setContent3Section(res.data.dataContent3);
         // this.setLayananSection(res.data.dataContent4);
         // this.konten5 = res.data.dataContent5.length == 0 ? {} : res.data.dataContent5[0];
         // this.tryOut = res.data.dataContent6.length == 0 ? {} : res.data.dataContent6[0];
@@ -192,7 +148,21 @@ export default {
       this.header.hero.ctaButtonText = content.tombol;
       this.header.hero.ctaButtonUrl = content.link;
       this.header.hero.image = content.gambar;
-    }
-  }
+    },
+    setContent3Section(contents) {
+      for (let indexContent = 0; indexContent < contents.length; indexContent++) {
+        if (contents[indexContent].id_content == 0) {
+          this.produk.judul = contents[indexContent].judul;
+          this.produk.deskripsi = contents[indexContent].text;
+        } else {
+          this.produk.item.push(contents[indexContent]);
+        }
+      }
+
+      for (let indexItem = 0; indexItem < this.produk.item.length; indexItem++) {
+        this.produk.item[indexItem].gambar = this.gambarProduk[indexItem];
+      }
+    },
+  },
 };
 </script>
