ce72dc5f6614ae1bfd190c8aeb2a66f4d79acc43
Angga Nurhiman <nurhimanangga@gmail.com>
2023-06-09 09:49:54 +0700

    feat(Tutor page CMS): Implementing API to update 2nd content

--- pages/administrator/edit-cms/untuk-tutor.vue
+++ pages/administrator/edit-cms/untuk-tutor.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col-md-12">  
+  <div class="col-md-12">
     <UIKonten>
       <template #title>Konten 1</template>
       <EditorContentMaster
@@ -13,15 +13,31 @@
     </UIKonten>
     <UIKonten>
       <template #title>Konten 2</template>
-      <EditorContentCard v-for="(card, i) in cards" :key="i" :content="card" />
-      <div class="col-md-12 pt-3 mt-1 row">
-        <button @click="addCardContent" class="btn btn-outline-primary">
-          + Tambah Sub Konten
-        </button>
-        <button @click="deleteCardContent" class="btn btn-outline-danger ml-3">
-          Hapus Sub Konten
-        </button>
+      <div class="row border-bottom">
+        <div class="form-user col-md-12">
+          <EditorText
+            v-model="carouselTitle.judul"
+            :initial-value="originalCarouselTitle.judul"
+            placeholder="Isi Judul baru"
+          >
+            <template #title>Judul</template>
+          </EditorText>
+        </div>
+        <div class="form-user col-md-12">
+          <EditorTextArea
+            v-model="carouselTitle.text"
+            :initial-value="originalCarouselTitle.text"
+          >
+            <template #title>Text</template>
+          </EditorTextArea>
+        </div>
       </div>
+      <EditorContentCard
+        v-for="(card, i) in carousel"
+        :key="i"
+        :content="card"
+        :original="originalCarousel[i]"
+      />
       <div class="col-md-12 pt-4">
         <button @click="saveCardContent" class="btn btn-primary">Simpan</button>
       </div>
@@ -57,16 +73,64 @@ export default {
           link: "",
         },
       ],
-      cards: [
+      originalCarouselTitle: {
+        id: "",
+        id_content: 0,
+        judul: "",
+        text: "",
+      },
+      carouselTitle: {
+        id: "",
+        id_content: 0,
+        judul: "",
+        text: "",
+      },
+      originalCarousel: [
         {
+          id: "",
+          id_content: 0,
           judul: "",
-          card: null,
-          posisiGambar: null,
+          text: "",
+        },
+        {
+          id: "",
+          id_content: 0,
+          judul: "",
+          text: "",
+        },
+        {
+          id: "",
+          id_content: 0,
+          judul: "",
+          text: "",
+        },
+      ],
+      carousel: [
+        {
+          id: "",
+          id_content: 1,
+          card: "",
+          judul: "",
+          text: "",
+        },
+        {
+          id: "",
+          id_content: 1,
+          card: "",
+          judul: "",
+          text: "",
+        },
+        {
+          id: "",
+          id_content: 1,
+          card: "",
+          judul: "",
+          text: "",
         },
       ],
     };
   },
-  methods: {    
+  methods: {
     async saveMasterContent() {
       if (typeof this.master[0].banner == "string") {
         this.master[0].banner = "";
@@ -98,18 +162,35 @@ export default {
         });
       }
     },
-    saveCardContent() {
-      console.log(this.cards);
-    },
-    addCardContent() {
-      this.cards.push({
-        judul: "",
-        card: null,
-        posisiGambar: null,
-      });
-    },
-    deleteCardContent() {
-      this.cards.pop();
+    async saveCardContent() {
+      for (let i = 0; i < this.carousel.length; i++) {
+        if (typeof this.carousel[i].carousel == "string") {
+          this.carousel[i].carousel = "";
+        }
+        this.carousel[i].card = this.carousel[i].carousel;
+      }
+      try {
+        const payload = objectToFormData({ konten2: { data: [...this.carousel, this.carouselTitle] } });
+        const res = await this.$axios.post("/api/cms/tutor", payload, {
+          headers: { "Content-Type": "multipart/form-data" },
+        });
+        if (res.data.success) {
+          this.getMainPageData();
+          this.$bvToast.toast("Berhasil mengubah konten", {
+            title: "Sukses",
+            variant: "success",
+            solid: true,
+            autoHideDelay: 3000,
+          });
+        }
+      } catch (e) {
+        this.$bvToast.toast("Gagal menyimpan konten", {
+          title: "Error",
+          variant: "danger",
+          solid: true,
+          autoHideDelay: 3000,
+        });
+      }
     },
     async getMainPageData() {
       const { data } = await this.$axios.get("/api/cms/tutor/get");
@@ -125,6 +206,31 @@ export default {
           this.master[0].text = this.originalMaster[0].text;
           this.master[0].tombol = this.originalMaster[0].tombol;
           this.master[0].link = this.originalMaster[0].link;
+        }
+
+        // Konten 2
+        let masterCarousel = data.data.dataContent2;
+        if (masterCarousel.length > 0) {
+          const title = masterCarousel.find((rev) => rev.id_content == 0);
+          this.originalCarouselTitle = title;
+          this.reviewCarousel.id = title.id;
+          this.reviewCarousel.judul = title.judul;
+          this.reviewCarousel.text = title.text;
+          masterCarousel.splice(
+            masterCarousel.findIndex((crsl) => crsl.id == title.id),
+            1
+          );
+
+          this.originalCarousel = masterCarousel;
+          for (let i = 0; i < masterCarousel.length; i++) {
+            if (!this.carousel[i]) {
+              this.carousel[i] = {};
+            }
+            this.carousel[i].id = masterCarousel[i].id;
+            this.carousel[i].carousel = masterCarousel[i].card;
+            this.carousel[i].judul = masterCarousel[i].judul;
+            this.carousel[i].text = masterCarousel[i].text;
+          }
         }
       }
     },
