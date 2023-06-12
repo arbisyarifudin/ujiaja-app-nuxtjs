457bef34f274d0c2eb79c491fe8c14d79c1df665
Angga Nurhiman <nurhimanangga@gmail.com>
2023-06-12 16:39:50 +0700

    feat(Orang tua page CMS): Implementing API to update 2nd content

--- components/Editor/Content/Card.vue
+++ components/Editor/Content/Card.vue
@@ -3,16 +3,18 @@
     <div class="row border-bottom">
       <div class="col-md-12">
         <EditorImage v-model="content.carousel" :max-size="5">
-          <template #title>
-            Card
-          </template>
+          <template #title> Card </template>
           <template #warn>
             *Disarankan dengan Banner 1276 x 638 pixel, dan Maksimal 5 Mb
           </template>
         </EditorImage>
       </div>
       <div class="form-user col-md-12">
-        <EditorText v-model="content.judul" :initial-value="original.judul" placeholder="Isi Judul baru">
+        <EditorText
+          v-model="content.judul"
+          :initial-value="original.judul"
+          placeholder="Isi Judul baru"
+        >
           <template #title>Judul</template>
         </EditorText>
       </div>
@@ -21,6 +23,29 @@
           <template #title>Text</template>
         </EditorTextArea>
       </div>
+      <div class="form-user col-md-12" v-if="content.posisi != undefined">
+        <label for="posisi">Posisi</label>
+        <b-form-select
+          id="posisi"
+          name="posisi"
+          class="form-control"
+          v-model="content.posisi"
+          :options="[
+            { text: '-- Pilih --', value: null },
+            { text: 'Kanan', value: 'kanan' },
+            { text: 'Kiri', value: 'kiri' },
+          ]"
+        >
+        </b-form-select>
+      </div>
+    </div>    
+    <div class="row py-2 border-bottom mt-5">
+      <button @click="$emit('add', content)" class="btn btn-outline-primary">
+        + Tambah Card
+      </button>
+      <button @click="$emit('delete', content)" class="btn btn-outline-danger ml-3" :disabled="isLast">
+        - Hapus Card
+      </button>
     </div>
   </div>
 </template>
@@ -30,12 +55,21 @@ export default {
   props: {
     content: {
       type: Object,
-      default: () => ({})
+      default: () => ({}),
     },
     original: {
       type: Object,
-      default: () => ({})
+      default: () => ({}),
+    },
+    isLast: {
+      type: Boolean,
+      default: false,
+    },
+    canAdd: {
+      type: Boolean,
+      default: false
     }
-  }
-}
+  },
+  emits: ['add', 'delete']
+};
 </script>
--- pages/administrator/edit-cms/untuk-orangtua.vue
+++ pages/administrator/edit-cms/untuk-orangtua.vue
@@ -2,18 +2,46 @@
   <div class="col-md-12">
     <UIKonten>
       <template #title>Konten 1</template>
-      <EditorContentMaster :content="master" />
+      <EditorContentMaster
+        :original="originalMaster"
+        :contents="master"
+        :can-add="false"
+      />
       <div class="col-md-12 pt-4">
         <button @click="saveMasterContent" class="btn btn-primary">Simpan</button>
       </div>
     </UIKonten>
     <UIKonten>
       <template #title>Konten 2</template>
-      <EditorContentCard v-for="(card, i) in cards" :key="i" :content="card" />
-      <div class="col-md-12 pt-3 mt-1 row">
-        <button @click="addCardContent" class="btn btn-outline-primary">+ Tambah Sub Konten</button>
-        <button @click="deleteCardContent" class="btn btn-outline-danger ml-3">Hapus Sub Konten</button>
+      <div class="row border-bottom">
+        <div class="form-user col-md-12">
+          <EditorText
+            v-model="carouselTitle.judul"
+            :initial-value="originalCarouselTitle.judul"
+            placeholder="Isi Judul baru"
+          >
+            <template #title>Judul</template>
+          </EditorText>
+        </div>
+        <div class="form-user col-md-12">
+          <EditorTextArea
+            v-model="carouselTitle.text"
+            :initial-value="originalCarouselTitle.text"
+          >
+            <template #title>Text</template>
+          </EditorTextArea>
+        </div>
       </div>
+      <EditorContentCard
+        v-for="(card, i) in carousel"
+        :key="i"
+        :content="card"
+        :original="originalCarousel[i]"
+        :is-last="carousel.length == 1"
+        :can-add="true"
+        @add="addCardContent"
+        @delete="deleteCardContent"
+      />
       <div class="col-md-12 pt-4">
         <button @click="saveCardContent" class="btn btn-primary">Simpan</button>
       </div>
@@ -31,9 +59,7 @@
         <div v-for="(t, i) in testimony.testimonies" :key="i" class="row border-bottom">
           <div class="col-md-12 pt-2">
             <EditorImage v-model="t.gambar" :max-size="5">
-              <template #title>
-                Gambar
-              </template>
+              <template #title> Gambar </template>
               <template #warn>
                 *Disarankan dengan Banner 1276 x 638 pixel, dan Maksimal 5 Mb
               </template>
@@ -60,13 +86,21 @@
             </EditorText>
             <p class="pt-3">Link</p>
             <div class="form-group row justify-content-between px-3">
-              <input v-model="t.link" placeholder="Isi Link yang ingin di tuju" class="form-control col-md-12" />
+              <input
+                v-model="t.link"
+                placeholder="Isi Link yang ingin di tuju"
+                class="form-control col-md-12"
+              />
             </div>
           </div>
         </div>
         <div class="row py-2 border-bottom">
-          <button @click="addTestimonyContent" class="btn btn-outline-primary">+ Tambah Testimoni</button>
-          <button @click="deleteTestimonyContent" class="btn btn-outline-danger ml-3">- Hapus Testimoni</button>
+          <button @click="addTestimonyContent" class="btn btn-outline-primary">
+            + Tambah Testimoni
+          </button>
+          <button @click="deleteTestimonyContent" class="btn btn-outline-danger ml-3">
+            - Hapus Testimoni
+          </button>
         </div>
       </div>
       <div class="col-md-12 pt-4">
@@ -77,111 +111,265 @@
 </template>
 
 <script>
-import objectToFormData from '../../../helpers/object-to-form-data'
+import objectToFormData from "../../../helpers/object-to-form-data";
 
 export default {
   data() {
     return {
-      master:{
-        banner: null,
-        gambar: null,
-        judul: '',
-        text: '',
-        sub_content: [
-          {
-            tombol: '',
-            link: ''
-          }
-        ]
-      },
-      cards: [
+      originalMaster: [
         {
-          judul: '',
-          card: null,
-          posisiGambar: null
-        }
+          id: "",
+          banner: "",
+          gambar: "",
+          judul: "",
+          text: "",
+          tombol: "",
+          link: "",
+        },
+      ],
+      master: [
+        {
+          id: "",
+          banner: "",
+          gambar: "",
+          judul: "",
+          text: "",
+          tombol: "",
+          link: "",
+        },
+      ],
+      originalCarouselTitle: {
+        id: "",
+        id_content: 0,
+        judul: "",
+        text: "",
+      },
+      carouselTitle: {
+        id: "",
+        id_content: 0,
+        judul: "",
+        text: "",
+      },
+      originalCarousel: [
+        {
+          id: "",
+          id_content: 0,
+          carousel: "",
+          judul: "",
+          text: "",
+          posisi: "",
+        },
+      ],
+      carousel: [
+        {
+          id: "",
+          id_content: 1,
+          carousel: "",
+          judul: "",
+          text: "",
+          posisi: "",
+        },
       ],
       testimony: {
         master: {
-          judul: ''
+          judul: "",
         },
         testimonies: [
           {
             gambar: null,
-            nama: '',
-            jurusan: '',
-            text: '',
-            tombol: '',
-            link: ''
-          }
-        ]
-      }
-    }
+            nama: "",
+            jurusan: "",
+            text: "",
+            tombol: "",
+            link: "",
+          },
+        ],
+      },
+    };
   },
   methods: {
-    saveMasterContent() {
-      console.log(this.master)
-    },
-    saveCardContent() {
-      console.log(this.cards)
-    },
-    addCardContent() {
-      this.cards.push({
-        judul: '',
-        card: null,
-        posisiGambar: null
-      })
-    },
-    deleteCardContent() {
-      this.cards.pop()
-    },
-    addTestimonyContent() {
-      this.testimony.testimonies.push({
-            gambar: null,
-            nama: '',
-            jurusan: '',
-            text: '',
-            tombol: '',
-            link: ''
-      })
-    },
-    deleteTestimonyContent() {
-      this.testimony.testimonies.pop()
-    },
     async saveMasterContent() {
+      if (typeof this.master[0].banner == "string") {
+        this.master[0].banner = "";
+      }
+      if (typeof this.master[0].gambar == "string") {
+        this.master[0].gambar = "";
+      }
       try {
-        const payload = objectToFormData({ konten1: this.master })
-        const res = await this.$axios.post('/api/cms/untuk-orangtua', payload, {
-          headers: { 'Content-Type': 'multipart/form-data' }
-        })
-        if (res.status === 200) window.alert("berhasil mengubah kontent")
+        const payload = objectToFormData({ konten1: { data: this.master } });
+        const res = await this.$axios.post("/api/cms/orang-tua", payload, {
+          headers: { "Content-Type": "multipart/form-data" },
+        });
+        if (res.data.success) {
+          this.getMainPageData();
+          this.$bvToast.toast("Berhasil mengubah konten", {
+            title: "Sukses",
+            variant: "success",
+            solid: true,
+            autoHideDelay: 3000,
+          });
+        }
       } catch (e) {
-        window.alert("gagal menyimpan konten")
+        console.log(e);
+        this.$bvToast.toast("Gagal menyimpan konten", {
+          title: "Error",
+          variant: "danger",
+          solid: true,
+          autoHideDelay: 3000,
+        });
       }
     },
     async saveCardContent() {
-      try {
-        const payload = objectToFormData({ konten2: this.cards })
-        const res = await this.$axios.post('/api/cms/untuk-orangtua', payload, {
-          headers: { 'Content-Type': 'multipart/form-data' }
-        })
-        if (res.status === 200) window.alert("berhasil mengubah kontent")
-      } catch (e) {
-        window.alert("gagal menyimpan konten")
+      for (let i = 0; i < this.carousel.length; i++) {
+        if (typeof this.carousel[i].carousel == "string") {
+          this.carousel[i].carousel = "";
+        }
+        this.carousel[i].card = this.carousel[i].carousel;
       }
+      try {
+        const payload = objectToFormData({
+          konten2: { data: [...this.carousel, this.carouselTitle] },
+        });
+        const res = await this.$axios.post("/api/cms/orang-tua", payload, {
+          headers: { "Content-Type": "multipart/form-data" },
+        });
+        if (res.data.success) {
+          this.getMainPageData();
+          this.$bvToast.toast("Berhasil mengubah konten", {
+            title: "Sukses",
+            variant: "success",
+            solid: true,
+            autoHideDelay: 3000,
+          });
+        }
+      } catch (e) {
+        console.log(e);
+        this.$bvToast.toast("Gagal menyimpan konten", {
+          title: "Error",
+          variant: "danger",
+          solid: true,
+          autoHideDelay: 3000,
+        });
+      }
+    },
+    addCardContent() {
+      this.carousel.push({
+        id: "",
+        id_content: 1,
+        carousel: "",
+        judul: "",
+        text: "",
+        posisi: "",
+      });
+      this.originalCarousel.push({
+        id: "",
+        id_content: 1,
+        carousel: "",
+        judul: "",
+        text: "",
+        posisi: "",
+      });
+    },
+    async deleteCardContent() {
+      try {
+        const lastData = this.carousel[this.carousel.length - 1];
+        if (lastData.id == "") {
+          this.carousel.pop();
+          return false;
+        }
+        const res = await this.$axios.post(
+          `/api/cms/orang-tua/delete-content2/${lastData.id}`,
+          {}
+        );
+        if (res.data.success) {
+          this.carousel.pop();
+          this.getMainPageData();
+          this.$bvToast.toast("Berhasil menghapus konten", {
+            title: "Sukses",
+            variant: "success",
+            solid: true,
+            autoHideDelay: 3000,
+          });
+        }
+      } catch (e) {
+        this.$bvToast.toast("Gagal menghapus konten", {
+          title: "Error",
+          variant: "danger",
+          solid: true,
+          autoHideDelay: 3000,
+        });
+      }
+    },
+    addTestimonyContent() {
+      this.testimony.testimonies.push({
+        gambar: null,
+        nama: "",
+        jurusan: "",
+        text: "",
+        tombol: "",
+        link: "",
+      });
+    },
+    deleteTestimonyContent() {
+      this.testimony.testimonies.pop();
     },
     async saveTestimonyContent() {
       try {
-        const payload = objectToFormData({ konten3: this.testimony
-         })
-        const res = await this.$axios.post('/api/cms/untuk-orangtua', payload, {
-          headers: { 'Content-Type': 'multipart/form-data' }
-        })
-        if (res.status === 200) window.alert("berhasil mengubah kontent")
+        const payload = objectToFormData({ konten3: this.testimony });
+        const res = await this.$axios.post("/api/cms/untuk-orangtua", payload, {
+          headers: { "Content-Type": "multipart/form-data" },
+        });
+        if (res.status === 200) window.alert("berhasil mengubah kontent");
       } catch (e) {
-        window.alert("gagal menyimpan konten")
+        window.alert("gagal menyimpan konten");
       }
     },
-  }
-}
+    async getMainPageData() {
+      const { data } = await this.$axios.get("/api/cms/orang-tua/get");
+      if (data.data instanceof Object) {
+        // Konten 1
+        const masterContent = data.data.dataContent1;
+        if (masterContent.length > 0) {
+          this.originalMaster = masterContent;
+          this.master[0].id = this.originalMaster[0].id;
+          this.master[0].banner = this.originalMaster[0].banner;
+          this.master[0].gambar = this.originalMaster[0].gambar;
+          this.master[0].judul = this.originalMaster[0].judul;
+          this.master[0].text = this.originalMaster[0].text;
+          this.master[0].tombol = this.originalMaster[0].tombol;
+          this.master[0].link = this.originalMaster[0].link;
+        }
+
+        // Konten 2
+        let masterCarousel = data.data.dataContent2;
+        if (masterCarousel.length > 0) {
+          const title = masterCarousel.find((rev) => rev.id_content == 0);
+          this.originalCarouselTitle = title;
+          this.reviewCarousel.id = title.id;
+          this.reviewCarousel.judul = title.judul;
+          this.reviewCarousel.text = title.text;
+          masterCarousel.splice(
+            masterCarousel.findIndex((crsl) => crsl.id == title.id),
+            1
+          );
+
+          this.originalCarousel = masterCarousel;
+          for (let i = 0; i < masterCarousel.length; i++) {
+            if (!this.carousel[i]) {
+              this.carousel[i] = {};
+            }
+            this.carousel[i].id = masterCarousel[i].id;
+            this.carousel[i].carousel = masterCarousel[i].card;
+            this.carousel[i].judul = masterCarousel[i].judul;
+            this.carousel[i].text = masterCarousel[i].text;
+            this.carousel[i].posisi = masterCarousel[i].posisi;
+          }
+        }
+      }
+    },
+  },
+  async mounted() {
+    await this.getMainPageData();
+  },
+};
 </script>
