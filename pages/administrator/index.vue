cba3cf233e7f2656f518dab9636bf862c6a4bdf9
Arbi Syarifudin <arbisyarifudin@gmail.com>
2024-04-21 21:08:32 +0700

    Add administrator/index.vue with redirect to dashboard

---
+++ pages/administrator/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    to administrator/index.vue
+  </div>
+</template>
+
+<script>
+// redirect to dashboard
+export default {
+  async asyncData({ redirect }) {
+    redirect('/administrator/dashboard')
+  }
+}
+</script>
